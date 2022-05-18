68f2ea87fa3fbe90bc05b5ded429812b66872abf
Keivan Tafakkori <91466697+Keivan-Tafakkori@users.noreply.github.com>
2022-05-18 15:40:26 +0430

    Update and rename Installing_a_package.py to Package_Installer.py

--- Package_Installer.py
+++ Package_Installer.py
@@ -1,9 +1,12 @@
+#Required package:
 import pip
 
+#Function:
 def install(package):
     if hasattr(pip, 'main'):
         pip.main(['install', package])
     else:
         pip._internal.main(['install', package])
-        
+     
+#Example:
 install('package name')
